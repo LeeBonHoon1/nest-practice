2622f2a62386419ea11d593316af8d5d1b4a9eb2
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-12-07 22:18:04 +0900

    db connect

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from 'src/boards/board-entity';
+import 'dotenv/config';
 
 export const typeORMConig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: 'postgres',
-  password: 'postgres',
-  database: 'board-app',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
   synchronize: true,
+  logging: true,
 };
